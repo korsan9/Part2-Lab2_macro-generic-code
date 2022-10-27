357dc9f8eb1dbf981ef0360d60e8b49cd3a58d0b
korsan9 <korsan.meriam9@gmail.com>
2022-10-27 09:55:06 +0100

    Exercise Solution Lab2 (version 2) main.c file

--- main .c
+++ main .c
@@ -4,20 +4,24 @@
   * @author  Meriam Korsan
   * @brief   Part2 Lab2_macro & generic code
   * @version V1.0.0
-  * @date    25-10-2022
+  * @date    27-10-2022
   *****************************************************************************/
 /* Includes ------------------------------------------------------------------*/
 
 /* Private function prototypes -----------------------------------------------*/
-static int mul (int var1, int var2);
-static int Compare_Max (int var1, int var2);
+
 
 /* Macros --------------------------------------------------------------------*/
 #define INTEL 1
 #define A 1
 #define B 3
-#define __SOM(X,Y) (INTEL)?(X+Y):0
-#define __COMPARE_MIN(X,Y) (INTEL)?((X<Y) ? X :Y) :0
+#define __Mul(X,Y) (X*Y)
+#define min(X, Y)   ((X) < (Y) ? (X) : (Y))      
+
+#if (INTEL)                                  
+  #define max(X, Y)  ((X) > (Y) ? (X) : (Y))
+  #define __SOM(X,Y) (X+Y)
+#endif
 #define OTHERS
 
 /* Private functions ---------------------------------------------------------*/
@@ -31,28 +35,22 @@ static int Compare_Max (int var1, int var2);
 
 int main(void)
 {
-  int som=__SOM(A,B);                 /*sum */
-  int Result_Min=__COMPARE_MIN(A,B);  /* min value between two numbers A and B*/
-  int MUX= mul(A,B);                  /*multiplication*/
-  int Result_Max= Compare_Max(A,B);   /* max value between two numbers A and B*/
+#ifdef OTHERS
 
- 
+  int MUX=__Mul(A,B);                  /*multiplication*/
+  int Result_Max= max(A,B);   /* max value between two numbers A and B*/
+
+#endif
+  int som=__SOM(A,B);                 /*sum */
+  int Result_Min=min(A,B);  /* min value between two numbers A and B*/
+
+
 
    while (1)
   {}
 }
 
 
-static int mul (int var1, int var2)
-{
-  return (var1*var2);                 /*multiplication*/
-}
-
-
-static int Compare_Max (int var1, int var2)
-{
-  return ((var1>var2)? var1:var2);    /* max value between two numbers A and B*/
-}
 
 
 
